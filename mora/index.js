75a16d988a6466367473f43fac447c9dbfaab004
xiuying545 <111640309+xiuying545@users.noreply.github.com>
2022-10-03 23:58:12 +0800

    Add files via upload

---
+++ mora/index.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import AppViews from './views/AppViews';
+import DeployerViews from './views/DeployerViews';
+import AttacherViews from './views/AttacherViews';
+import {renderDOM, renderView} from './views/render';
+import './index.css';
+import * as backend from './build/index.main.mjs';
+import { loadStdlib } from '@reach-sh/stdlib';
+const reach = loadStdlib(process.env);
+
+const thumbToInt={'ZERO':0,'ONE':1,'TWO':2,'THREE':3,'FOUR':4,'FIVE':5};
+const guessToInt={'ZEROs':0,'ONEs':1,'TWOs':2,'THREEs':3,'FOURs':4,'FIVEs':5,'SIXs':6,'SEVENs':7,'EIGHTs':8,'NINEs':9,'TENs':10};
+const intToOutcome=['Alice_WINS', 'TIE', 'Bob_WINS'];
+const {standardUnit} = reach;
+const defaults = {defaultFundAmt: '10', defaultWager: '3', standardUnit};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {view: 'ConnectAccount', ...defaults};
+  }
+  async componentDidMount() {
+    const acc = await reach.getDefaultAccount();
+    const balAtomic = await reach.balanceOf(acc);
+    const bal = reach.formatCurrency(balAtomic, 4);
+    this.setState({acc, bal});
+    if (await reach.canFundFromFaucet()) {
+      this.setState({view: 'FundAccount'});
+    } else {
+      this.setState({view: 'DeployerOrAttacher'});
+    }
+  }
+  async fundAccount(fundAmount) {
+    await reach.fundFromFaucet(this.state.acc, reach.parseCurrency(fundAmount));
+    this.setState({view: 'DeployerOrAttacher'});
+  }
+  async skipFundAccount() { this.setState({view: 'DeployerOrAttacher'}); }
+  selectAttacher() { this.setState({view: 'Wrapper', ContentView: Attacher}); }
+  selectDeployer() { this.setState({view: 'Wrapper', ContentView: Deployer}); }
+  render() { return renderView(this, AppViews); }
+}
+
+class Player extends React.Component {
+  random() { return reach.hasRandom.random(); }
+
+  async getGuess(){
+      const guess=await new Promise(resolveNumber =>{
+              this.setState({views:'GetGuess', playable: true, resolveHandP});
+      });
+      this.setState({view: 'WaitingForResults', thumb});
+      return thumbToInt[thumb];
+  }
+  async getThumb() { // Fun([], UInt)
+    const thumb = await new Promise(resolveHandP => {
+      this.setState({view: 'GetThumb', playable: true, resolveHandP});
+    });
+    this.setState({view: 'WaitingForResults', thumb});
+    return thumbToInt[thumb];
+  }
+  seeOutcome(i) { this.setState({view: 'Done', outcome: intToOutcome[i]}); }
+  informTimeout() { this.setState({view: 'Timeout'}); }
+  playHand(thumb) { this.state.resolveHandP(thumb); }
+}
+
+class Deployer extends Player {
+  constructor(props) {
+    super(props);
+    this.state = {view: 'SetWager'};
+  }
+  setWager(wager) { this.setState({view: 'Deploy', wager}); }
+  async deploy() {
+    const ctc = this.props.acc.contract(backend);
+    this.setState({view: 'Deploying', ctc});
+    this.wager = reach.parseCurrency(this.state.wager); // UInt
+    this.deadline = {ETH: 10, ALGO: 100, CFX: 1000}[reach.connector]; // UInt
+    backend.Alice(ctc, this);
+    const ctcInfoStr = JSON.stringify(await ctc.getInfo(), null, 2);
+    this.setState({view: 'WaitingForAttacher', ctcInfoStr});
+  }
+  render() { return renderView(this, DeployerViews); }
+}
+class Attacher extends Player {
+  constructor(props) {
+    super(props);
+    this.state = {view: 'Attach'};
+  }
+  attach(ctcInfoStr) {
+    const ctc = this.props.acc.contract(backend, JSON.parse(ctcInfoStr));
+    this.setState({view: 'Attaching'});
+    backend.Bob(ctc, this);
+  }
+  async acceptWager(wagerAtomic) { // Fun([UInt], Null)
+    const wager = reach.formatCurrency(wagerAtomic, 4);
+    return await new Promise(resolveAcceptedP => {
+      this.setState({view: 'AcceptTerms', wager, resolveAcceptedP});
+    });
+  }
+  termsAccepted() {
+    this.state.resolveAcceptedP();
+    this.setState({view: 'WaitingForTurn'});
+  }
+  render() { return renderView(this, AttacherViews); }
+}
+
+renderDOM(<App />);
